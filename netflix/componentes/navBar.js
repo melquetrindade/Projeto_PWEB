fb33f3b2d59b612702f8369db357f3a5c0423189
melquetrindade <melquetrindade654@gmail.com>
2023-10-23 11:52:08 -0300

    continuando o  desenvolvimento do nav bar

--- netflix/componentes/navBar.js
+++ netflix/componentes/navBar.js
@@ -6,29 +6,35 @@ export default function NavBar(){
 
     const [inputShow, setInput] = useState('close')
     const searchToggle = () => {
+        console.log('entrou')
         inputShow == 'open' ? setInput('close') : setInput('open')
     }
 
     return(
-        <ul className={styles.ul}>
-            <li>
-                <span class="material-symbols-outlined">menu</span>
-            </li>
-            <li>
-                <Link href='/'>HOME</Link>
-            </li>
-            <li>
-                <Link href='/filmes'>FILMES</Link>
-            </li>
-            <li>
-                <Link href='/series'>SÉRIES</Link>
-            </li>
-            <li>
+        <div className={styles.navBar}>
+            <div>
+                <div className={styles.itemMenu}>
+                    <span class="material-symbols-outlined">menu</span>
+                    <ul className={styles.ul}>
+                        <li>
+                            <Link href='/'>HOME</Link>
+                        </li>
+                        <li>
+                            <Link href='/filmes'>FILMES</Link>
+                        </li>
+                        <li>
+                            <Link href='/series'>SÉRIES</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.logo}>
+                    <img src="/logo_filme.jpg"/>
+                </div>
                 <div className={inputShow == 'close' ? styles.searchClose : styles.searchOpen}>
                     <span class="material-symbols-outlined" onClick={searchToggle}>search</span>
                     <input type="text" placeholder="Spider-Man" className={inputShow == 'open' ? styles.open : styles.close}></input>
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
     )
 }
