297d16b87958fb5fb218ab9fd59c5b14c27c3b8f
melquetrindade <melquetrindade654@gmail.com>
2023-11-01 14:55:08 -0300

    iniando a criação da página de artistas

--- netflix/pages/artists.js
+++ netflix/pages/artists.js
@@ -4,8 +4,11 @@ import styles from '../styles/artists.module.css'
 export default function Artists(){
     return(
         <main className={styles.body}> 
-            <div>
-                <h1>Página de Artistas</h1>
+            <div className={styles.main}>
+                <h1 className={styles.title}>Artistas</h1>
+                <div className={styles.containerArtists}>
+                    <p>Conteúdo vem aqui</p>
+                </div>
             </div>
         </main>
     )
