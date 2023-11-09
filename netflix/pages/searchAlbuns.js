d41756200d681f73aea6e85147da2d6cab7a187b
melquetrindade <melquetrindade654@gmail.com>
2023-11-09 15:11:07 -0300

    continuando o desenvolvimento do site

--- netflix/pages/searchAlbuns.js
+++ netflix/pages/searchAlbuns.js
@@ -46,34 +46,41 @@ export default function SearchArtists(){
 
     return(
         <main className={styles.body}>
-            <div>
-                {contextHolder}
-                <div className={styles.content}>
-                    <h1 className="text-center py-10">Pesquise por Álbuns</h1>
-                    <div class='form-floating'>
-                        <input 
-                            type="text" 
-                            id="searchAlbuns" 
-                            class="form-control shadow-none" 
-                            required
-                            value={valueInput}
-                            placeholder="TBT WS" 
-                            minlength="1" 
-                            maxlength="26"
-                            onChange={handleChangeSearch}
-                            onKeyPress={handleKeyPress}
-                        >
-                        </input>
-                        <label for="searchArtists">Digite o Nome do Álbum</label>
-                    </div>
-                    <div className={styles.instrucoes}>
-                        <p>Atenção: As pesquisas devem ser feitas sem o uso de acentos ou caracteres especiais!</p>
-                    </div>
-                </div>
-                <button className={styles.buttonSearch} onClick={navAlbuns}>
-                    <span class="material-symbols-outlined">search</span>
-                </button>
-            </div>
+            <Formulario arg={contextHolder} func={handleChangeSearch} func01={handleKeyPress} func02={navAlbuns} teste={valueInput}/>
         </main>
+    )
+}
+
+function Formulario({arg, func, func01, func02, teste}){
+
+    return(
+        <div>
+            {arg}
+            <div className={styles.content}>
+                <h1 className="text-center py-10">Pesquise por Álbuns</h1>
+                <div class='form-floating'>
+                    <input 
+                        type="text" 
+                        id="searchAlbuns" 
+                        class="form-control shadow-none" 
+                        required
+                        value={teste}
+                        placeholder="TBT WS" 
+                        minlength="1" 
+                        maxlength="26"
+                        onChange={func}
+                        onKeyPress={func01}
+                    >
+                    </input>
+                    <label for="searchArtists">Digite o Nome do Álbum</label>
+                </div>
+                <div className={styles.instrucoes}>
+                    <p>Atenção: As pesquisas devem ser feitas sem o uso de acentos ou caracteres especiais!</p>
+                </div>
+            </div>
+            <button className={styles.buttonSearch} onClick={func02}>
+                <span class="material-symbols-outlined">search</span>
+            </button>
+        </div>
     )
 }
--- netflix/pages/searchArtists.js
+++ netflix/pages/searchArtists.js
@@ -6,7 +6,7 @@ import {notification} from 'antd'
 export default function SearchArtists(){
 
     const [valueInput, setInput] = useState('')
-    
+
     const [api, contextHolder] = notification.useNotification();
     const openNotification = ({placement, title}) => {
         api.info({
@@ -18,11 +18,11 @@ export default function SearchArtists(){
     }
 
     const router = useRouter()
-    const navArtists = () => {
-        if(document.getElementById('searchArtists').value){
+    const navAlbuns = () => {
+        if(document.getElementById('searchAlbuns').value){
             router.push({
                 pathname: './artists',
-                query: {artista: document.getElementById('searchArtists').value}
+                query: {artista: document.getElementById('searchAlbuns').value}
             })
         }
         else{
@@ -33,49 +33,84 @@ export default function SearchArtists(){
     const handleChangeSearch = (e) => {
         const inputText = e.target.value
 
-        if (!(/^[a-zA-Z 0-9 ']+$/.test(inputText)) || !(inputText === '')) {
+        if (/^[a-zA-Z 0-9 ']+$/.test(inputText) || inputText === '') {
             setInput(inputText)
         }
-        
-        
     }
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
-            navArtists()
+            navAlbuns()
         }
     };
 
     return(
         <main className={styles.body}>
-            <div>
-                {contextHolder}
-                <div className={styles.content}>
-                    <h1 className="text-center py-10">Pesquise por Artistas</h1>
-                    <div class='form-floating'>
-                        <input 
-                            type="text" 
-                            id="searchArtists" 
-                            class="form-control shadow-none" 
-                            required
-                            value={valueInput}
-                            placeholder="Marília Mendonça" 
-                            minlength="1" 
-                            maxlength="26"
-                            onChange={handleChangeSearch}
-                            onKeyPress={handleKeyPress}
-                        >
-                        </input>
-                        <label for="searchArtists">Digite o Nome do Artista</label>
-                    </div>
-                    <div className={styles.instrucoes}>
-                        <p>Atenção: As pesquisas devem ser feitas sem o uso de acentos ou caracteres especiais!</p>
-                    </div>
-                </div>
-                <button className={styles.buttonSearch} onClick={navArtists}>
-                    <span class="material-symbols-outlined">search</span>
-                </button>
-            </div>
+            <Formulario arg={contextHolder} func={handleChangeSearch} func01={handleKeyPress} func02={navAlbuns} teste={valueInput}/>
         </main>
     )
 }
+
+function Formulario({arg, func, func01, func02, teste}){
+
+    return(
+        <div>
+            {arg}
+            <div className={styles.content}>
+                <h1 className="text-center py-10">Pesquise por Álbuns</h1>
+                <div class='form-floating'>
+                    <input 
+                        type="text" 
+                        id="searchAlbuns" 
+                        class="form-control shadow-none" 
+                        required
+                        value={teste}
+                        placeholder="TBT WS" 
+                        minlength="1" 
+                        maxlength="26"
+                        onChange={func}
+                        onKeyPress={func01}
+                    >
+                    </input>
+                    <label for="searchArtists">Digite o Nome do Álbum</label>
+                </div>
+                <div className={styles.instrucoes}>
+                    <p>Atenção: As pesquisas devem ser feitas sem o uso de acentos ou caracteres especiais!</p>
+                </div>
+            </div>
+            <button className={styles.buttonSearch} onClick={func02}>
+                <span class="material-symbols-outlined">search</span>
+            </button>
+        </div>
+    )
+}
+
+/*
+<div>
+    {contextHolder}
+    <div className={styles.content}>
+        <h1 className="text-center py-10">Pesquise por Álbuns</h1>
+        <div class='form-floating'>
+            <input 
+                type="text" 
+                id="searchAlbuns" 
+                class="form-control shadow-none" 
+                required
+                value={valueInput}
+                placeholder="TBT WS" 
+                minlength="1" 
+                maxlength="26"
+                onChange={handleChangeSearch}
+                onKeyPress={handleKeyPress}
+            >
+            </input>
+            <label for="searchArtists">Digite o Nome do Álbum</label>
+        </div>
+        <div className={styles.instrucoes}>
+            <p>Atenção: As pesquisas devem ser feitas sem o uso de acentos ou caracteres especiais!</p>
+        </div>
+    </div>
+    <button className={styles.buttonSearch} onClick={navAlbuns}>
+        <span class="material-symbols-outlined">search</span>
+    </button>
+</div>*/
